10d66fe972f7cd72d890be1ed85c323aa6526992
Pablo Silva <pablonicolassilvabravo@gmail.com>
2019-10-01 21:16:04 -0300

    Avances en login

--- server/server.js
+++ server/server.js
@@ -10,7 +10,9 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(require('./routes/usuario'))
+
+// Configuración routing
+app.use(require('./routes/index'))
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     (err, res) => {
