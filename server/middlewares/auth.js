c617cf924303ec86938e7fad4896459ebc4b889a
Pablo Silva <pablonicolassilvabravo@gmail.com>
2019-10-01 22:01:45 -0300

    se agrega validacion de token en endpoints

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -11,7 +11,9 @@ let verificaToken = (req, res, next) => {
         if (err) {
             return res.status(401).json({
                 ok: false,
-                err
+                err: {
+                    message: 'Token no válido'
+                }
             })
         }
         req.usuario = decoded.usuario
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -11,9 +11,9 @@ const app = express();
 
 app.get('/usuario', verificaToken, (req, res) => {
 
+
     let desde = req.query.desde || 0;
     let limite = req.query.limite || 5;
-
 
     desde = Number(desde);
     limite = Number(limite);
@@ -41,7 +41,7 @@ app.get('/usuario', verificaToken, (req, res) => {
         })
 })
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', verificaToken, (req, res) => {
     let body = req.body;
 
     let usuario = new Usuario({
@@ -70,7 +70,7 @@ app.post('/usuario', function(req, res) {
 })
 
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', verificaToken, (req, res) => {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
@@ -93,7 +93,7 @@ app.put('/usuario/:id', function(req, res) {
 })
 
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', verificaToken, (req, res) => {
     let id = req.params.id;
 
     req.body.estado = 'false';
