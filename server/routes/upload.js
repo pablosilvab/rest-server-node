fca1da942007571b0eebc94baedc6f26db1cc0f4
pablosilvab <pablonicolassilvabravo@gmail.com>
2019-11-05 21:40:22 -0300

    subir imagenes de produtos y control duplicidad

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -3,6 +3,10 @@ const fileUpload = require('express-fileupload');
 const app = express();
 
 const Usuario = require('../models/usuario');
+const Producto = require('../models/producto');
+
+const fs = require('fs');
+const path = require('path');
 
 app.use(fileUpload({ useTempFiles: true }));
 
@@ -46,24 +50,107 @@ app.put('/upload/:tipo/:id', function(req, res) {
 
 
     // Nombre unico para el archivo
-    let nombre = `${id}-${new Date().getMilliseconds()}.${extension}`;
+    let nombreImagen = `${id}-${new Date().getMilliseconds()}.${extension}`;
 
 
     // Use the mv() method to place the file somewhere on your server
-    archivo.mv(`uploads/${tipo}/${nombre}`, (err) => {
+    archivo.mv(`uploads/${tipo}/${nombreImagen}`, (err) => {
         if (err) {
             return res.status(500).json({
                 ok: false,
                 err
             });
-
         }
 
-        return res.json({
-            ok: true,
-            message: 'Imagen subida exitosamente'
-        });
+        // Imagen cargada.
+        switch (tipo) {
+            case 'usuarios':
+                imagenUsuario(id, res, nombreImagen);
+            case 'productos':
+                imagenProducto(id, res, nombreImagen);
+        }
     });
 });
+
+function imagenUsuario(id, res, nombreImagen) {
+    Usuario.findById(id, (err, usuarioDB) => {
+        if (err) {
+            eliminarArchivo(nombreImagen, usuarioDB.img);
+
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioDB) {
+            eliminarArchivo('usuarios', usuarioDB.img);
+
+            return res.status(400).json({
+
+                ok: false,
+                err: {
+                    message: 'Usuario no existe'
+                }
+            });
+        }
+
+        eliminarArchivo('usuarios', usuarioDB.img);
+
+        usuarioDB.img = nombreImagen;
+        usuarioDB.save((err, usuarioNew) => {
+            res.json({
+                ok: true,
+                usuario: usuarioNew
+            });
+        });
+
+
+    });
+}
+
+function imagenProducto(id, res, nombreImagen) {
+    Producto.findById(id, (err, productoDB) => {
+        if (err) {
+            eliminarArchivo(nombreImagen, productoDB.img);
+
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!productoDB) {
+            eliminarArchivo('productos', productoDB.img);
+
+            return res.status(400).json({
+
+                ok: false,
+                err: {
+                    message: 'Producto no existe'
+                }
+            });
+        }
+
+        eliminarArchivo('productos', productoDB.img);
+
+        productoDB.img = nombreImagen;
+        productoDB.save((err, productoNew) => {
+            res.json({
+                ok: true,
+                producto: productoNew
+            });
+        });
+
+
+    });
+}
+
+function eliminarArchivo(tipo, nombreImagen) {
+    let pathImg = path.resolve(__dirname, `../../uploads/${tipo}/${nombreImagen}`);
+    if (fs.existsSync(pathImg)) {
+        fs.unlinkSync(pathImg);
+    }
+}
 
 module.exports = app;
