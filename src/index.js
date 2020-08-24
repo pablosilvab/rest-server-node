6450a30d6cd525f5a6e61d7f31f4d01dd7a82eff
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-08-24 00:49:32 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 require('./config/config')
 
-
 const express = require('express')
-
 const mongoose = require('mongoose');
 const path = require('path');
-const app = express()
 const bodyParser = require('body-parser')
 
-
+const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -18,7 +15,6 @@ app.use(express.static(path.resolve(__dirname, './public')));
 
 // Configuración routing
 app.use(require('./routes/index'))
-
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     (err, res) => {
