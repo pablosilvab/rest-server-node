bf727231cc951b680d958848b58bff14d3d665b4
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-08-23 23:41:54 -0400

    add jest test

---
+++ tests/index.test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+describe('sum two values', () => {
+    test('sum two values', () => {
+        const a =1
+        const b = 2
+        const expected = 3
+        const result = a+b
+        expect(result).toBe(expected)
+    });
+});
